975bb7e5489b63de3b554df448ed0c2b2a2da048
nt3752 <30674439+nt3752@users.noreply.github.com>
2026-02-11 14:39:30 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shot-tracker-v26";
+const CACHE_NAME = "shot-tracker-v27";
 const ASSETS = ["./","./index.html","./manifest.json"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
