a8a4ca3f9bb5af0d892a53de21af3a452a8b123f
nt3752 <30674439+nt3752@users.noreply.github.com>
2026-02-11 14:19:22 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,11 @@
-const CACHE_NAME = "shot-tracker-v25";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./service-worker.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting();
+const CACHE_NAME = "shot-tracker-v26";
+const ASSETS = ["./","./index.html","./manifest.json"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
