a2af3121099a6cdd7b65a22e7af57dcf219a87fe
nt3752 <30674439+nt3752@users.noreply.github.com>
2026-02-14 09:40:05 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.SHOT_TRACKER_VERSION = "v36_20"; console.log("Shot Tracker", window.SHOT_TRACKER_VERSION);
+window.SHOT_TRACKER_VERSION = "v36_21"; console.log("Shot Tracker", window.SHOT_TRACKER_VERSION);
 
 
 // ---- Distance unit helpers (yards internal, feet for putter) ----
